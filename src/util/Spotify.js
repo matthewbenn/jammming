6667db85b67a038a120c784566b5254e5e982d27
Matthew Benn <matthewbenn@gmail.com>
2018-08-16 07:08:15 -0500

    moved location of const endpoint, page renders

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -6,7 +6,6 @@
 
 const authorizeUrl = `https://accounts.spotify.com/authorize?`;
 const responseUrl = window.location.href
-const endpoint = `${authorizeUrl}&${client_id}&${response_type}&${redirect_uri}&${state}&${scope}`
 
 // Data to be sent
 const client_id = 'dd31a42fc3154ed88c65c8a2c3a87c0c';
@@ -24,19 +23,20 @@ let expires_in = '';
 
 const Spotify = {
 
-const getAccessToken() {
+ getAccessToken() {
   if (access_token) {
     return access_token;
   } else if (responseUrl.match(/access_token=([^&]*)/) &&
-             reponseUrl.match(/expires_in=([^&]*)/) &&
+             responseUrl.match(/expires_in=([^&]*)/) &&
              responseUrl.match(/token_type=([^&]*)/)){
     access_token = responseUrl.match(/access_token=([^&]*)/);
-    expires_in = reponseUrl.match(/expires_in=([^&]*)/);
+    expires_in = responseUrl.match(/expires_in=([^&]*)/);
     token_type = responseUrl.match(/token_type=([^&]*)/);
     return access_token;
-    return expires_in;
-    return token_type;
+    window.setTimeout(() => access_token = '', expires_in * 1000);
+    window.history.pushState('Access Token', null, '/');
   } else {
+    const endpoint = `${authorizeUrl}&${client_id}&${response_type}&${redirect_uri}&${state}&${scope}`
     window.location = endpoint;
   }
     // pull it from the URL
