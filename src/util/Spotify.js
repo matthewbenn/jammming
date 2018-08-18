65a08f18afd12e97f088ea327e4f16e5ef413784
Matthew Benn <matthewbenn@gmail.com>
2018-08-18 16:38:08 -0500

    a little cleanup match returns array, so need to pull from array value

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -31,45 +31,47 @@ const Spotify = {
   } else if (responseUrl.match(/access_token=([^&]*)/) &&
              responseUrl.match(/expires_in=([^&]*)/) &&
              responseUrl.match(/token_type=([^&]*)/)) {
-    access_token = responseUrl.match(/access_token=([^&]*)/);
-    expires_in = responseUrl.match(/expires_in=([^&]*)/);
-    token_type = responseUrl.match(/token_type=([^&]*)/);
+     let access_token_array = responseUrl.match(/access_token=([^&]*)/);
+     let expires_in_array = responseUrl.match(/expires_in=([^&]*)/);
+     access_token = access_token_array[1];
+     const expiresIn = Number(expires_in_array[1]);
     return access_token;
     window.setTimeout(() => access_token = '', expires_in * 1000);
     window.history.pushState('Access Token', null, '/');
   } else {
     const endpoint = `${authorizeUrl}client_id=${client_id}&response_type=${response_type}&redirect_uri=${redirect_uri}&state=${state}&scope=${scope}`
     window.location = endpoint;
-  } 
+  }
 },
 
 search(term) {
 accessToken = this.getAccessToken();
-  fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`,{
-  headers: {
-    Authorization: `Bearer ${accessToken}`
-  }
-}).then(response => {
-  if (response.ok) {
-    return response.json();
-  } throw new Error ('Request failed!');
-}).then(networkError => {
-  console.log(networkError.message);
-}).then(jsonResponse => {
-  if (jsonResponse.tracks) {
-    return jsonResponse.tracks.map(track => {
-      return {
-                  id: track.id,
-                  album: track.album.name,
-                  artist: track.artists[0].name,
-                  name:track.name,
-                  uri: track.uri
-                };
-              });
+  fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`
+    }
+  }).then(response => {
+    if (response.ok) {
+      return response.json();
+    } throw new Error ('Request failed!');
+  }).then(networkError => {
+    console.log(networkError.message);
+  }).then(jsonResponse => {
+    if (!jsonResponse.tracks) {
+      return [];
+    } return jsonResponse.tracks.items.map(track => ({
+
+                    id: track.id,
+                    album: track.album.name,
+                    artist: track.artists[0].name,
+                    name:track.name,
+                    uri: track.uri
+
+                  }));
+                }
+              )
             }
-          })
-       }
-     };
+          };
 export default Spotify;
 /* original first-pass code below
 search(term) {
